c7ae0f76255cdd085885f770ab5b73092700829d
stefanolinden <stefanolindenbojm@gmail.com>
2024-02-20 07:41:17 -0300

    commitv

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 def extract_route(request):
     l = request.split()
     for i in l:
@@ -10,5 +12,22 @@ def extract_route(request):
 def read_file(Path):
     arquivo = open(Path, 'rb').read()
     return arquivo
+
+def load_data(nome):
+    path = './data/' + nome
+    arquivo = open(path, 'r').read()
+    data = json.loads(arquivo)   
+    return data
+        
+def load_template(nome):
+    path = './templates/' + nome
+    with open(path, 'r') as arquivo:
+        data = arquivo.read()
+    return data
+        
+            
+    
+    
+    
 
     
---
+++ views.py
@@ -0,0 +1,13 @@
+from utils import load_data, load_template
+
+def index():
+    # Cria uma lista de <li>'s para cada anotação
+    # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+    note_template = load_template('components/note.html')
+    notes_li = [
+        note_template.format(title=dados['titulo'], details=dados['detalhes'])
+        for dados in load_data('notes.json')
+    ]
+    notes = '\n'.join(notes_li)
+
+    return load_template('index.html').format(notes=notes).encode()
